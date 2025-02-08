0a86d88f3d61e1ce9b56f47f0a9855d2dac105df
Lex Stapleton <lex.stapleton1423@gmail.com>
2025-02-08 11:29:21 -0500

    debugging camera collisions

--- chill-out/scenes/testing/camera_controller.gd
+++ chill-out/scenes/testing/camera_controller.gd
@@ -1,7 +1,8 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var scroll_speed : float = 0
 @export var follow_rate : float = 0 
+@export var min_follow_rate : float = 0
 @export var max_follow_distance : float = 0
 @export var min_follow_distance : float = 0
 
@@ -13,6 +14,8 @@ var player : Node2D = null
 
 @export var panning = false
 @export var pan_target : Vector2 = Vector2(0, 0)
+
+var clamping_enabled : bool = false
 
 signal done_panning
 
@@ -33,18 +36,34 @@ func smooth_follow(delta: float):
 		return
 		
 	var to_player = player.global_position - global_position
+	
+	var collision : KinematicCollision2D = move_and_collide(to_player)
+	
+	if collision:
+		print(to_player, collision.get_travel())
+		to_player = collision.get_travel()
+		print("detected collision at ", collision.get_position())
+		
+	
 	var dist = to_player.length()
 	
 	if dist < min_follow_distance:
-		position += to_player
+		return
+		velocity = to_player
+		print("jumping ", velocity)
 		
 	elif dist > max_follow_distance:
-		position += ((dist - max_follow_distance)/dist) * to_player
+		velocity = ((dist - max_follow_distance)/dist) * to_player
 	
 	else:
-		global_position = global_position.lerp(player.global_position, clamp(delta*follow_rate, min_pan_rate, 1))
-
+		print(delta * follow_rate)
+		velocity = to_player * clamp(delta*follow_rate, 0, 1)
+		print("following ", velocity)
+	
+	move_and_slide()
+	velocity = Vector2(0, 0)
 func start_panning(target: Vector2):
+	$CollisionShape2D.disabled = true
 	pan_target = target
 	panning = true
 	
@@ -54,13 +73,13 @@ func pan(delta):
 	var dist = to_target.length()
 	
 	if dist < min_follow_distance:
-		position = pan_target
+		global_position = pan_target
 		panning = false
+		$CollisionShape2D.disabled = false
 		done_panning.emit()
 	
 	else:
 		global_position = global_position.lerp(pan_target, delta*pan_rate)
-		
 
 func _physics_process(delta: float) -> void:
 	if panning:
