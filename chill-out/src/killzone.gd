b8bed9b0d608951ce6d0a0b918acc26ad28df851
Lex Stapleton <lex.stapleton1423@gmail.com>
2025-02-09 15:06:49 -0500

    added player detection

--- chill-out/src/killzone.gd
+++ chill-out/src/killzone.gd
@@ -3,6 +3,5 @@ extends Area2D
 signal player_died
 
 func _on_body_entered(body: Node2D) -> void:
-	print("body entered kill zone: ", body.name)
 	if body.is_in_group("player"):
 		player_died.emit()
--- chill-out/src/level.gd
+++ chill-out/src/level.gd
@@ -1,5 +1,12 @@
 class_name Level extends Node2D
 
+signal player_enter()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_enter.emit(self)
+
+
 func get_camera_bounds() -> Rect2:
 	return Rect2($CameraBounds.global_position - $CameraBounds.shape.size, 2*$CameraBounds.shape.size)
 
