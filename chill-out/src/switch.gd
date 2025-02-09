3f383ffbcb997d7372f933b7f87f77be5dcc5189
Lex Stapleton <lex.stapleton1423@gmail.com>
2025-02-09 14:37:39 -0500

    levers and gates are working

--- chill-out/src/switch.gd
+++ chill-out/src/switch.gd
@@ -3,12 +3,15 @@ extends Area2D
 signal open_gate
 signal close_gate
 
+@export var open_duration : float = 10
 @onready var timer = $Timer
 
 func activate():
+	$Sprite2D.frame = 1
 	open_gate.emit()
-	timer.start()
+	timer.start(open_duration)
 
 
 func _on_timer_timeout() -> void:
+	$Sprite2D.frame = 0
 	close_gate.emit()
