77727e946014a0d0846a1e6b436c6bd5279f4e10
Shaydas1 <43556419+Shaydas1@users.noreply.github.com>
2025-02-09 14:23:18 -0500

    Swapped out test tileset for real tileset

--- chill-out/src/level.gd
+++ chill-out/src/level.gd
@@ -1,7 +1,5 @@
 class_name Level extends Node2D
 
-@export var spawn_point: Vector2 = Vector2(0, 0)
-
 func get_camera_bounds() -> Rect2:
 	return Rect2($CameraBounds.global_position - $CameraBounds.shape.size, 2*$CameraBounds.shape.size)
 
