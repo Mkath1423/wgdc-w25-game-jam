a31d5efcd3101da687cad33a65ac2bf8e96692fd
Lex Stapleton <lex.stapleton1423@gmail.com>
2025-02-09 15:29:24 -0500

    fixed respawn

--- chill-out/src/level.gd
+++ chill-out/src/level.gd
@@ -1,10 +1,13 @@
 class_name Level extends Node2D
 
-signal player_enter()
+
+func _ready():
+	$Area2D.body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		player_enter.emit(self)
+		get_tree().get_root().get_child(0).set_active_room(get_parent())
+
 
 
 func get_camera_bounds() -> Rect2:
